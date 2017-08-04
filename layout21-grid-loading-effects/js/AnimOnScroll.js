8bac2710cb1acd4e9cab6d684686148b53b80cfd
Matthew Choi <matthewchoi7@gmail.com>
2017-08-04 15:05:52 +1000

    layout21-grid-loading-effects with css grid

--- layout21-grid-loading-effects/js/AnimOnScroll.js
+++ layout21-grid-loading-effects/js/AnimOnScroll.js
@@ -96,10 +96,10 @@
 			imagesLoaded( this.el, function() {
 				
 				// initialize masonry
-				new Masonry( self.el, {
-					itemSelector: 'li',
-					transitionDuration : 0
-				} );
+				// new Masonry( self.el, {
+				// 	itemSelector: 'li',
+				// 	transitionDuration : 0
+				// } );
 				
 				if( Modernizr.cssanimations ) {
 					// the items already shown...
